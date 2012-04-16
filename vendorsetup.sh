c31b8346c88f6e8d37c0b3102f97a2fb7c47268d
Randy Dyck <leoisright@gmail.com>
2012-04-15 20:11:22 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
+add_lunch_combo classic_epic4gtouch-eng-not-ready-yet
 add_lunch_combo classic_otter-eng
 add_lunch_combo classic_shooter-eng
-add_lunch_combo classic_skyrocket-eng-not ready yet
+add_lunch_combo classic_skyrocket-eng-not-ready-yet
 add_lunch_combo classic_supersonic-eng
 add_lunch_combo classic_tenderloin-eng
 
